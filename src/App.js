7e54364b7e7719c2f6cb6737b654ee9b57059d91
Andrea Lee <33338925+and-lee@users.noreply.github.com>
2020-06-28 15:26:57 -0400

    Added button ripple light and ripple dark

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from "react";
 import "../examples/example.css";
 import "./main.scss";
 import WButtonPage from "../examples/WButtonPage";
+import WNavbarPage from "../examples/WNavbarPage";
 
 export default function App() {
   return (
     <div>
       <WButtonPage />
+      <WNavbarPage />
     </div>
   );
 }
--- src/components/wbutton/WButton.js
+++ src/components/wbutton/WButton.js
@@ -3,7 +3,7 @@ import "./wbutton.scss";
 
 const WButton = ({ children, className, onClick, style }) => {
   return (
-    <button onClick={onClick} className={`wbtn ${className}`} style={style}>
+    <button onClick={onClick} className={`wbutton ${className}`} style={style}>
       {children}
     </button>
   );
